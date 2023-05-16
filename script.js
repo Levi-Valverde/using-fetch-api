dd7723a8c3ce0c59d4f4b5a6c5ceaf49435b9ef4
levi <levivalverdelima@gmail.com>
2023-05-16 16:54:44 -0300

    Finishing files

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ async function getAllPost() {
   console.log(data);
 
   loadinElement.classList.add("hide");
+
+  data.map((post) => {
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const link = document.createElement("a");
+
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = "Ler";
+    link.setAttribute("href", `/post.html?id=${post.id}`);
+
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+
+    postContainer.appendChild(div);
+  });
 }
 
 getAllPost();
